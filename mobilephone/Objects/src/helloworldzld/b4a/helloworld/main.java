c86c3f048d91864af1e1fdd02b821192fc12f673
zhanglongde <1039326495@qq.com>
2016-03-17 20:57:39 +0800

    1st

---
+++ mobilephone/Objects/src/helloworldzld/b4a/helloworld/main.java
@@ -0,0 +1,271 @@
+package helloworldzld.b4a.helloworld;
+
+import anywheresoftware.b4a.B4AMenuItem;
+import android.app.Activity;
+import android.os.Bundle;
+import anywheresoftware.b4a.BA;
+import anywheresoftware.b4a.BALayout;
+import anywheresoftware.b4a.B4AActivity;
+import anywheresoftware.b4a.ObjectWrapper;
+import anywheresoftware.b4a.objects.ActivityWrapper;
+import java.lang.reflect.InvocationTargetException;
+import anywheresoftware.b4a.B4AUncaughtException;
+import anywheresoftware.b4a.debug.*;
+import java.lang.ref.WeakReference;
+
+public class main extends Activity implements B4AActivity{
+	static main mostCurrent;
+	static boolean afterFirstLayout;
+	static boolean isFirst = true;
+    private static boolean processGlobalsRun = false;
+	BALayout layout;
+	public static BA processBA;
+	BA activityBA;
+    ActivityWrapper _activity;
+    java.util.ArrayList<B4AMenuItem> menuItems;
+	private static final boolean fullScreen = false;
+	private static final boolean includeTitle = true;
+    public static WeakReference<Activity> previousOne;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		if (isFirst) {
+			processBA = new BA(this.getApplicationContext(), null, null, "helloworldzld.b4a.helloworld", "main");
+			processBA.loadHtSubs(this.getClass());
+	        float deviceScale = getApplicationContext().getResources().getDisplayMetrics().density;
+	        BALayout.setDeviceScale(deviceScale);
+		}
+		else if (previousOne != null) {
+			Activity p = previousOne.get();
+			if (p != null && p != this) {
+                anywheresoftware.b4a.keywords.Common.Log("Killing previous instance (main).");
+				p.finish();
+			}
+		}
+		if (!includeTitle) {
+        	this.getWindow().requestFeature(android.view.Window.FEATURE_NO_TITLE);
+        }
+        if (fullScreen) {
+        	getWindow().setFlags(android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN,   
+        			android.view.WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        }
+		mostCurrent = this;
+        processBA.activityBA = null;
+		layout = new BALayout(this);
+		setContentView(layout);
+		afterFirstLayout = false;
+		BA.handler.postDelayed(new WaitForLayout(), 5);
+
+	}
+	private static class WaitForLayout implements Runnable {
+		public void run() {
+			if (afterFirstLayout)
+				return;
+			if (mostCurrent.layout.getWidth() == 0) {
+				BA.handler.postDelayed(this, 5);
+				return;
+			}
+			mostCurrent.layout.getLayoutParams().height = mostCurrent.layout.getHeight();
+			mostCurrent.layout.getLayoutParams().width = mostCurrent.layout.getWidth();
+			afterFirstLayout = true;
+			mostCurrent.afterFirstLayout();
+		}
+	}
+	private void afterFirstLayout() {
+		activityBA = new BA(this, layout, processBA, "helloworldzld.b4a.helloworld", "main");
+        processBA.activityBA = new java.lang.ref.WeakReference<BA>(activityBA);
+        _activity = new ActivityWrapper(activityBA, "activity");
+        anywheresoftware.b4a.Msgbox.isDismissing = false;
+        initializeProcessGlobals();		
+        initializeGlobals();
+        anywheresoftware.b4a.objects.ViewWrapper.lastId = 0;
+        anywheresoftware.b4a.keywords.Common.Log("** Activity (main) Create, isFirst = " + isFirst + " **");
+        processBA.raiseEvent2(null, true, "activity_create", false, isFirst);
+		isFirst = false;
+		if (mostCurrent == null || mostCurrent != this)
+			return;
+        processBA.setActivityPaused(false);
+        anywheresoftware.b4a.keywords.Common.Log("** Activity (main) Resume **");
+        processBA.raiseEvent(null, "activity_resume");
+
+	}
+	public void addMenuItem(B4AMenuItem item) {
+		if (menuItems == null)
+			menuItems = new java.util.ArrayList<B4AMenuItem>();
+		menuItems.add(item);
+	}
+	@Override
+	public boolean onCreateOptionsMenu(android.view.Menu menu) {
+		super.onCreateOptionsMenu(menu);
+		if (menuItems == null)
+			return false;
+		for (B4AMenuItem bmi : menuItems) {
+			android.view.MenuItem mi = menu.add(bmi.title);
+			if (bmi.drawable != null)
+				mi.setIcon(bmi.drawable);
+			mi.setOnMenuItemClickListener(new B4AMenuItemsClickListener(bmi.eventName.toLowerCase(BA.cul)));
+		}
+		return true;
+	}
+	private class B4AMenuItemsClickListener implements android.view.MenuItem.OnMenuItemClickListener {
+		private final String eventName;
+		public B4AMenuItemsClickListener(String eventName) {
+			this.eventName = eventName;
+		}
+		public boolean onMenuItemClick(android.view.MenuItem item) {
+			processBA.raiseEvent(item.getTitle(), eventName + "_click");
+			return true;
+		}
+	}
+    public static Class<?> getObject() {
+		return main.class;
+	}
+    private Boolean onKeySubExist = null;
+    private Boolean onKeyUpSubExist = null;
+	@Override
+	public boolean onKeyDown(int keyCode, android.view.KeyEvent event) {
+		if (onKeySubExist == null)
+			onKeySubExist = processBA.subExists("activity_keypress");
+		if (onKeySubExist) {
+			Boolean res =  (Boolean)processBA.raiseEvent2(_activity, false, "activity_keypress", false, keyCode);
+			if (res == null || res == true)
+				return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+    @Override
+	public boolean onKeyUp(int keyCode, android.view.KeyEvent event) {
+		if (onKeyUpSubExist == null)
+			onKeyUpSubExist = processBA.subExists("activity_keyup");
+		if (onKeyUpSubExist) {
+			Boolean res =  (Boolean)processBA.raiseEvent2(_activity, false, "activity_keyup", false, keyCode);
+			if (res == null || res == true)
+				return true;
+		}
+		return super.onKeyUp(keyCode, event);
+	}
+	@Override
+	public void onNewIntent(android.content.Intent intent) {
+		this.setIntent(intent);
+	}
+    @Override 
+	public void onPause() {
+		super.onPause();
+        if (_activity == null) //workaround for emulator bug (Issue 2423)
+            return;
+		anywheresoftware.b4a.Msgbox.dismiss(true);
+        anywheresoftware.b4a.keywords.Common.Log("** Activity (main) Pause, UserClosed = " + activityBA.activity.isFinishing() + " **");
+        processBA.raiseEvent2(_activity, true, "activity_pause", false, activityBA.activity.isFinishing());		
+        processBA.setActivityPaused(true);
+        mostCurrent = null;
+        if (!activityBA.activity.isFinishing())
+			previousOne = new WeakReference<Activity>(this);
+        anywheresoftware.b4a.Msgbox.isDismissing = false;
+	}
+
+	@Override
+	public void onDestroy() {
+        super.onDestroy();
+		previousOne = null;
+	}
+    @Override 
+	public void onResume() {
+		super.onResume();
+        mostCurrent = this;
+        anywheresoftware.b4a.Msgbox.isDismissing = false;
+        if (activityBA != null) { //will be null during activity create (which waits for AfterLayout).
+        	ResumeMessage rm = new ResumeMessage(mostCurrent);
+        	BA.handler.post(rm);
+        }
+	}
+    private static class ResumeMessage implements Runnable {
+    	private final WeakReference<Activity> activity;
+    	public ResumeMessage(Activity activity) {
+    		this.activity = new WeakReference<Activity>(activity);
+    	}
+		public void run() {
+			if (mostCurrent == null || mostCurrent != activity.get())
+				return;
+			processBA.setActivityPaused(false);
+            anywheresoftware.b4a.keywords.Common.Log("** Activity (main) Resume **");
+		    processBA.raiseEvent(mostCurrent._activity, "activity_resume", (Object[])null);
+		}
+    }
+	@Override
+	protected void onActivityResult(int requestCode, int resultCode,
+	      android.content.Intent data) {
+		processBA.onActivityResult(requestCode, resultCode, data);
+	}
+	private static void initializeGlobals() {
+		processBA.raiseEvent2(null, true, "globals", false, (Object[])null);
+	}
+
+public anywheresoftware.b4a.keywords.Common __c = null;
+public static int _mynumber = 0;
+public anywheresoftware.b4a.objects.EditTextWrapper _edittext1 = null;
+public static String  _activity_create(boolean _firsttime) throws Exception{
+ //BA.debugLineNum = 16;BA.debugLine="Sub Activity_Create(FirstTime As Boolean)";
+ //BA.debugLineNum = 20;BA.debugLine="Activity.LoadLayout(\"guess\")";
+mostCurrent._activity.LoadLayout("guess",mostCurrent.activityBA);
+ //BA.debugLineNum = 21;BA.debugLine="MyNumber=Rnd(1,5)";
+_mynumber = anywheresoftware.b4a.keywords.Common.Rnd((int)(1),(int)(5));
+ //BA.debugLineNum = 22;BA.debugLine="End Sub";
+return "";
+}
+public static String  _activity_pause(boolean _userclosed) throws Exception{
+ //BA.debugLineNum = 38;BA.debugLine="Sub Activity_Pause (UserClosed As Boolean)";
+ //BA.debugLineNum = 40;BA.debugLine="End Sub";
+return "";
+}
+public static String  _activity_resume() throws Exception{
+ //BA.debugLineNum = 34;BA.debugLine="Sub Activity_Resume";
+ //BA.debugLineNum = 36;BA.debugLine="End Sub";
+return "";
+}
+public static String  _btnguess_click() throws Exception{
+ //BA.debugLineNum = 24;BA.debugLine="Sub btnGuess_CLick";
+ //BA.debugLineNum = 25;BA.debugLine="If EditText1.Text>MyNumber Then";
+if ((double)(Double.parseDouble(mostCurrent._edittext1.getText()))>_mynumber) { 
+ //BA.debugLineNum = 26;BA.debugLine="ToastMessageShow(\"My number is samller.\",False)";
+anywheresoftware.b4a.keywords.Common.ToastMessageShow("My number is samller.",anywheresoftware.b4a.keywords.Common.False);
+ }else if((double)(Double.parseDouble(mostCurrent._edittext1.getText()))<_mynumber) { 
+ //BA.debugLineNum = 28;BA.debugLine="ToastMessageShow(\"My number is larger.\",False)";
+anywheresoftware.b4a.keywords.Common.ToastMessageShow("My number is larger.",anywheresoftware.b4a.keywords.Common.False);
+ }else {
+ //BA.debugLineNum = 30;BA.debugLine="ToastMessageShow(\"well done\",True)";
+anywheresoftware.b4a.keywords.Common.ToastMessageShow("well done",anywheresoftware.b4a.keywords.Common.True);
+ };
+ //BA.debugLineNum = 32;BA.debugLine="EditText1.SelectAll";
+mostCurrent._edittext1.SelectAll();
+ //BA.debugLineNum = 33;BA.debugLine="End Sub";
+return "";
+}
+
+public static void initializeProcessGlobals() {
+    
+    if (processGlobalsRun == false) {
+	    processGlobalsRun = true;
+		try {
+		        main._process_globals();
+		
+        } catch (Exception e) {
+			throw new RuntimeException(e);
+		}
+    }
+}
+public static String  _globals() throws Exception{
+ //BA.debugLineNum = 9;BA.debugLine="Sub Globals";
+ //BA.debugLineNum = 12;BA.debugLine="Dim MyNumber As Int";
+_mynumber = 0;
+ //BA.debugLineNum = 13;BA.debugLine="Dim EditText1 As EditText";
+mostCurrent._edittext1 = new anywheresoftware.b4a.objects.EditTextWrapper();
+ //BA.debugLineNum = 14;BA.debugLine="End Sub";
+return "";
+}
+public static String  _process_globals() throws Exception{
+ //BA.debugLineNum = 2;BA.debugLine="Sub Process_Globals";
+ //BA.debugLineNum = 6;BA.debugLine="End Sub";
+return "";
+}
+}
